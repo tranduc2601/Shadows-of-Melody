44559390e5b1a7b3e65bef2f7a4ff06c3cb7f1f0
VirusBetaTester <duyhoangtran2006@gmail.com>
2026-03-31 08:02:32 +0700

    UI_Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  srcDir: './src/frontend',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
